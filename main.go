065e7752bc356ed512864d079d3613f361a6bc6e
carlso70 <carlso70@purdue.edu>
2018-04-16 17:18:24 -0400

    Add test handler for recieve message

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"strings"
 
+	"bitbucket.org/ckvist/twilio/twiml"
+	"bitbucket.org/ckvist/twilio/twirest"
 	"github.com/gorilla/mux"
 )
 
@@ -16,49 +17,42 @@ var (
 	TWILO_ACCOUNT = os.Getenv("TWILO_ACCOUNT")
 	TWILO_NUM     = os.Getenv("TWILO_NUM")
 	URL_STR       = strings.Join([]string{"https://api.twilio.com/2010-04-01/Accounts/", TWILO_ACCOUNT, "/Messages.json"}, "")
+	twilioClient  = twirest.NewClient(
+		TWILO_ACCOUNT,
+		TWILO_KEY)
 )
 
 func sendMessage(number_to, message string) {
-	msgData := url.Values{}
-	msgData.Set("To", number_to)
-	msgData.Set("From", TWILO_NUM)
-	msgData.Set("Body", message)
-	msgDataReader := *strings.NewReader(msgData.Encode())
-
-	client := &http.Client{}
-	req, _ := http.NewRequest("POST", URL_STR, &msgDataReader)
-	req.SetBasicAuth(TWILO_ACCOUNT, TWILO_KEY)
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	resp, _ := client.Do(req)
-	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
-		var data map[string]interface{}
-		decoder := json.NewDecoder(resp.Body)
-		err := decoder.Decode(&data)
-		if err == nil {
-			fmt.Println(data["sid"])
-		}
-	} else {
-		fmt.Println(resp.Status)
+	msg := twirest.SendMessage{
+		Text: message,
+		From: TWILO_NUM,
+		To:   number_to,
 	}
+
+	twilioClient.Request(msg)
 }
 
 func smsRecieve(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	text := vars["text"]
+	sender := r.FormValue("From")
+	body := r.FormValue("Body")
 
-	fmt.Fprintf(w, "Recieved the text: %s\n", text)
+	resp := twiml.NewResponse()
+	resp.Action(twiml.Message{
+		Body: fmt.Sprintf("Hello, %s, you said: %s", sender, body),
+		From: TWILO_NUM,
+		To:   sender,
+	})
+	resp.Send(w)
+}
+
+func test(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello there human!")
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/smsRecieve", smsRecieve).Methods("GET")
+	r.HandleFunc("/sms/recieve", smsRecieve)
+	r.HandleFunc("/", test)
 
-	http.ListenAndServe(":80", r)
-
-	fmt.Println("Running Server....")
-
-	// send test message
-	sendMessage("+18477215493", "test message")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
